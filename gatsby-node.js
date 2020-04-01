3ef250a371175175e2ee0d13364a10b0003e2ced
Tracie Twite <traciecodes@gmail.com>
2020-04-01 11:25:29 -0500

    Added img sourcing for blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,5 @@
 const path = require("path")
 
-module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
-
-  //extract slugs for blogpost urls:
-  if (node.internal.type === "MarkdownRemark") {
-    const slug = path.basename(node.fileAbsolutePath, ".md")
-
-    // add the new field onto the node:
-    createNodeField({
-      node,
-      name: "slug",
-      value: slug,
-    })
-  }
-}
-
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   // get path to template:
@@ -23,28 +7,24 @@ module.exports.createPages = async ({ graphql, actions }) => {
   // get markdown data:
   const res = await graphql(`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost {
         edges {
           node {
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
     }
   `)
 
-  // iterate over posts and run template on each instance:
-  res.data.allMarkdownRemark.edges.forEach(edge => {
+  // iterate over posts and run template on each instance to create new pages:
+  res.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
       component: blogTemplate,
-      path: `/blog/${edge.node.fields.slug}`,
+      path: `/blog/${edge.node.slug}`,
       context: {
-        slug: edge.node.fields.slug,
+        slug: edge.node.slug,
       },
     })
   })
-
-  // create new pages:
 }
